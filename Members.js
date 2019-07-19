2ff648b01f5e5b918106e70cf8d12d3cc826f9a8
Muhammad Umar Khan <umar.khan@arbisoft.com>
2019-07-19 16:25:30 +0500

    Crash course for express.js

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [  
+    {
+        id: 1,
+        name: 'Johon Doe1',
+        email: 'john1@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Johon Doe2',
+        email: 'john2@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Johon Doe3',
+        email: 'john3@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Johon Doe4',
+        email: 'john4@gmail.com',
+        status: 'inactive'
+    }
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+const exphbs = require('express-handlebars');
+const app = express();
+
+
+
+// initialize middleware
+app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Boady Parser Middleware (Available in new version of express)
+app.use(express.json());
+// To handle url encoded data
+app.use(express.urlencoded({ extended: false }));
+
+// Hompage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members // this is same as members: members
+}));
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>  console.log(`Server started on port ${PORT}`));
